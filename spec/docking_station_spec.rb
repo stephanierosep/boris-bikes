7b46ce8dd93ee3435f3a7851921cfcdc6dfa87c7
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-10-30 15:35:36 +0000

    Defining release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
-require 'docking_station'
+require 'docking_station.rb'
 
 describe DockingStation do
+  docking_station = DockingStation.new
+  it "responds to the method release_bike" do
+    expect(docking_station).to respond_to "release_bike"
+  end
 end
