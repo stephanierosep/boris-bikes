66bc45e82ffd84d466e2c49a8d8826521d18701c
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-10-30 16:18:08 +0000

    feature tested that we can get a bike from docking station and check it's working

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,10 @@
 class DockingStation
   def release_bike
+    Bike.new
+  end
+end
+
+class Bike
+  def working?
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,12 @@ describe DockingStation do
   it "responds to the method release_bike" do
     expect(docking_station).to respond_to "release_bike"
   end
+
+
+  it "gets a bike and checks that it is working?" do
+    bike = docking_station.release_bike
+    expect(bike).not_to eq(nil)
+    expect(bike).to respond_to("working?")
+  end
+  
 end
