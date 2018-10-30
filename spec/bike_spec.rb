2ed83adf866293b848ec939c8874253aae6b2bb7
Al <al@Als-MacBook-Pro.local>
2018-10-30 15:55:20 +0000

    changed bike_spec to use one-liner syntax

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,7 +2,5 @@ require 'bike.rb'
 
 describe Bike do
   bike = Bike.new
-  it 'responds to the method "working?"' do
-    expect(bike).to respond_to('working?')
-  end
+  it { expect(bike).to respond_to('working?') }
 end
