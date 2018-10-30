f53b33ff9eb3b4911c325f813c8f422b3e4fccae
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-10-30 15:07:51 +0000

    setting up rspec

---
+++ errors.rb
@@ -0,0 +1,7 @@
+=begin
+ Type of error : NameError (uninitialized constant DockingStation)
+ File Path : /Users/stephanieparker/.rvm/rubies/ruby-2.5.1/bin/irb:11:in
+
+=end
+
+docking_station = DockingStation.new
